b6e94db0825992c7fcc1051f5c7a570baf669efd
JoxZ <jandresmurillo2605@gmail.com>
2025-04-18 15:45:09 -0600

    Update MusiGest.cpp

--- MusiGest.cpp
+++ MusiGest.cpp
@@ -176,29 +176,93 @@ Artistas* buscarArtistas(string ide){
     }
     return NULL;
 }
+SellosDiscograficos* buscarSellos(string ide) {
+    if (primerS == NULL) {
+        return NULL;
+    }
+    SellosDiscograficos* temp = primerS;
+    do {
+        if (temp->id == ide) { 
+            return temp;
+        }
+        temp = temp->siguiente;
+    } while (temp != primerS); 
+    return NULL;
+}
+
+/*Sello discográfico*/
+void insertarSellosDiscográficos(string id,string nombre, int pais,int añoFund){
+    SellosDiscograficos* newSello = new SellosDiscograficos(id,nombre,pais,añoFund);
+    if (primerS == NULL){
+        primerS = newSello;
+        newSello -> siguiente = newSello;
+        newSello -> anterior = newSello;
+    }else{
+        SellosDiscograficos* temp = primerS -> anterior;
+        temp -> siguiente = newSello;
+        newSello -> anterior = temp;
+        newSello -> siguiente = primerS;
+        primerS -> anterior = newSello;
+    }
+}
+
+void modificarNombredeSello(string ide,string newNombre){
+    SellosDiscograficos* SelloDisc = buscarSellos(ide);
+    if (SelloDisc == NULL){
+        cout << "El sello discográfico no existe"<<endl;
+    }else{
+    SelloDisc -> nombre = newNombre;
+    cout << "Nombre del sello discográfico modificado correctamente"<<endl;
+    }
+}
+
+void modificarPaisdeSello(string ide,string newPais){
+    SellosDiscograficos* SelloDisc = buscarSellos(ide);
+    if (SelloDisc == NULL){
+        cout << "El sello discográfico no existe"<<endl;
+    }else{
+        SelloDisc -> pais = newPais;
+        cout << "País del sello discográfico modificado correctamente"<<endl;
+    }
+}
+void eliminarSelloDiscografico(string ide) {
+    SellosDiscograficos* selloDisc = buscarSellos(ide);
+    if (selloDisc == NULL) {
+        cout << "El sello discográfico no está registrado"<<endl;
+        return;
+    }
+    if (selloDisc->siguiente == selloDisc && selloDisc->anterior == selloDisc) {
+        delete selloDisc;
+        primerS = NULL;
+    }
+    else {
+        selloDisc->anterior->siguiente = selloDisc->siguiente;
+        selloDisc->siguiente->anterior = selloDisc->anterior;
+
+        if (selloDisc == primerS) {
+            primerS = selloDisc->siguiente; 
+        }
+        delete selloDisc;
+    }
+}
 
 /* ARTISTAS */
 void insertarOrdenAlfabeticamente(string id, string nombreArt, string nombreReal, string pais, string selloDisc) {
-
     if (buscarArtistas(id) != NULL) {
         cout << "El artista con el ID " << id << " ya existe." << endl;
         return;
     }
-
     struct Artistas *nuevoArtista = new Artistas(id, nombreArt, nombreReal, pais, selloDisc);
-
     if (primerA == NULL) {
         primerA = nuevoArtista;
         return;
     }
-
     if (nombreArt < primerA -> nombreArtistico) {
         nuevoArtista -> siguiente = primerA;
         primerA -> anterior = nuevoArtista;
         primerA = nuevoArtista;
         return;
     }
-
     Artistas *temp = primerA;
     while (temp -> siguiente != NULL && temp -> siguiente -> nombreArtistico < nombreArt) {
         temp = temp -> siguiente;
@@ -295,7 +359,6 @@ void reporteArtistas() {
         }
     }
 }
-
 
 int main()
 {
