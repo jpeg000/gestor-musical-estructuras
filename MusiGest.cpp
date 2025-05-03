1119659cd9e5a9734beb5e9111d950aa6e9b5edc
JoxZ <jandresmurillo2605@gmail.com>
2025-05-02 21:17:35 -0600

    Update MusiGest.cpp

--- MusiGest.cpp
+++ MusiGest.cpp
@@ -1260,3 +1260,60 @@ void imprimirAlbumesConCancionesOrdenadasPorDuracion() {
 }
 
 
+
+
+
+
+/*MENÚ*/
+void menuGeneral() {
+    bool salir = false;
+
+    cout << "=== Bienvenido al Menú General De MusiGest ===" << endl;
+
+    while (!salir) {
+        int opcion = 0;
+
+        cout << "1. Mantenimiento" << endl;
+        cout << "2. Consultas" << endl;
+        cout << "3. Reportes" << endl;
+        cout << "4. Salir del Programa" << endl;
+        cout << "Ingrese una opción: ";
+        cin >> opcion;
+        cout << endl;
+
+        switch (opcion) {
+            case 1: {
+                menuMantenimiento();
+                break;
+            }
+
+            case 2: {
+                menuConsultas();
+                break;
+            }
+
+            case 3: {
+                menuReportes();
+                break;
+            }
+
+            case 4: {
+                cout << "-- Se ha salido del programa exitosamente --" << endl;
+                salir = true;
+                break;
+            }
+
+            default: {
+                cout << "Instrucción inválida. Intente nuevamente." << endl;
+                break;
+            }
+        }
+    }
+}
+
+
+int main() {
+    menuGeneral();
+    return 0;
+}
+
